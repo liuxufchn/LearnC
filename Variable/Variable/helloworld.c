b62fa1b6977cffb801f85c1806f793e2058a4fa0
liuxufchn <liuxufchn@163.com>
2023-06-19 23:31:05 +0800

    commit

--- Variable/Variable/helloworld.c
+++ Variable/Variable/helloworld.c
@@ -1,8 +1,37 @@
-#include <stdio.h>
+#include <stdio.h> // #：关键标识符，表示引入头文件；include：引入头文件关键字
+					// stdio.h：系统标准输入、输出库对应的头文件
+					// <>：使用系统库函数。 ""：用户自定义库函数
+
+int main_hello(void)
+{
+	printf("hello,world\n");
+
+	system("cmd");
+
+	system("calc");
+
+	system("pause");
+
+	return 0;
+}
+
 
 int main(void)
 {
-	printf("hello,world\n");
+	int a;
+	int b;
+	int c;
+
+	__asm
+	{
+		mov a, 3
+		mov b, 4
+		mov eax, a
+		add eax, b
+		mov c, eax
+	}
+
+	printf("%d\n", c);
 
 	return 0;
 }
